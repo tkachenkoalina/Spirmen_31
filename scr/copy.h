4fdb64d7cc75c300c57c62a04078f7752c85b28c
tkachenkoalina <61449283+tkachenkoalina@users.noreply.github.com>
2020-05-18 23:29:57 +0300

    Create copy.h

---
+++ scr/copy.h
@@ -0,0 +1,6 @@
+#ifndef 31_Tkachenko_Alina_COPY_H
+#define 31_Tkachenko_Alina_COPY_H
+
+void copy(int * first_arr, int * second_arr, int size_other_arr, int start_index_first_arr, int start_index_second_arr);
+
+#endif //31_Tkachenko_Alina_COPY_H
